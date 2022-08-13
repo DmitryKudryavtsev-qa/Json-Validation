b960656df4e90ee11e02856de93b1a2b1f9e4747
Dmitry Kudryavtsev <108889879+DmitryKudryavtsev-qa@users.noreply.github.com>
2022-08-13 16:47:35 +0300

    Add files via upload

---
+++ json_bot.py
@@ -0,0 +1,54 @@
+
+ # подключение библиотеки telebot
+# В google colab добавить: !pip install pyTelegramBotAPI
+# для установки необходимо в файл requirements.text добавить строку
+# 'PyTelegramBotApi'
+from telebot import TeleBot, types
+import json
+
+bot = TeleBot(token='5504202926:AAGH2iUOJqFfVMQ18L2HZvONfSmlwXIT6zU', parse_mode='html') # создание бота
+
+
+# обработчик команды '/start'
+@bot.message_handler(commands=['start'])
+def start_command_handler(message: types.Message):
+    # отправляем ответ на команду '/start'
+    bot.send_message(
+        chat_id=message.chat.id, # id чата, в который необходимо направить сообщение
+        text=f'Привет {message.from_user.first_name}! Я могу проверить твой JSON и отформатировать его в красивый текст\nВведи JSON в виде строки:', # текст сообщения
+        parse_mode='Html')
+
+# обработчик всех остальных сообщений
+@bot.message_handler()
+def message_handler(message: types.Message):
+    try:
+        # пытаемся распарсить JSON из текста сообщения
+        payload = json.loads(message.text)
+    except json.JSONDecodeError as ex:
+        # при ошибке взникнет исключение 'json.JSONDecodeError'
+        # преобразовываем исключение в строку и выводим пользователю
+        bot.send_message(
+            chat_id=message.chat.id,
+            text=f'УПС!!! Соррян, какой то косяк:\n<code>{str(ex)}</code>'
+        )
+        # выходим из функции
+        return
+    
+    # если исключения не возникло - значит был введен корректный JSON
+    # форматируем его в красивый текст :) (отступ 2 пробела на уровень, сортировать ключи по алфавиту)
+    text = json.dumps(payload, indent=2, sort_keys=True, ensure_ascii=False)
+    # и выводим пользователю
+    bot.send_message(
+        chat_id=message.chat.id,
+        text=f'Все отлично, ты крут! JSON:\n<code>{text}</code>'
+    )
+
+
+# главная функция программы
+def main():
+    # запускаем нашего бота
+    bot.infinity_polling()
+
+
+if __name__ == '__main__':
+        main()
